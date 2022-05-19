31661a3f9a4e34e8efed75b3c934a9579d432e77
Nataliasilvalima <nataliasilvalima2014@gmail.com>
2022-05-18 21:34:54 -0300

    inicio teste

---
+++ src/Teste.java
@@ -0,0 +1,8 @@
+import Dados.Pessoa;
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa);
+    }
+}
